091254699558d03e347785b482b39b03e31b6592
Wu <jianan.w@compute1-client-1.ris.wustl.edu>
2026-01-11 18:18:34 -0600

    Running test5 and others

--- submit.sh
+++ submit.sh
@@ -4,11 +4,11 @@
 #BSUB -gpu "num=1"
 #BSUB -a "docker(jiananwu72/miniconda-cupy)"
 # #BSUB -oo simulation_log.txt
-#BSUB -eo simulation_err.txt
+#BSUB -eo simulation_err_LuFe.txt
 
 export HOME=/tmp
 
 export CUDA_PATH=/opt/conda/envs/apdd
 export CPATH=$CUDA_PATH/include:$CPATH
 
-/opt/conda/envs/apdd/bin/python scripts/simulation.py
+/opt/conda/envs/apdd/bin/python scripts/test_LuFe.py
